d2a392ddb23f25561f12dec81360c5a01566b237
Souler <juanjoseherrero@playtomic.io>
2020-10-03 20:07:27 +0200

    feat: add scheduled posts fix: fouc on load
    closes #7

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -6,6 +6,7 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
+import Scheduled from "../components/Scheduled"
 
 type Data = {
   site: {
@@ -20,6 +21,7 @@ type Data = {
         frontmatter: {
           title: string
           date: string
+          date_iso: string
           description: string
         }
         fields: {
@@ -41,27 +43,29 @@ const BlogIndex = ({ data, location }: PageProps<Data>) => {
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
+          <Scheduled dueDate={node.frontmatter.date_iso as any} fallback={<></>}>
+            <article key={node.fields.slug}>
+              <header>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </section>
+            </article>
+          </Scheduled>
         )
       })}
     </Layout>
@@ -85,6 +89,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
+            date_iso: date
             date(formatString: "MMMM DD, YYYY")
             title
             description
